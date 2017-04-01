0f55962721e4ad7f98d127b55e75f3bc54e12bf4
luan nguyen <luan nguyen>
2017-04-01 15:08:16 +0700

    Hoan thanh trang chu

---
+++ PixelShop/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace PixelShop
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                        "~/Scripts/jquery-1.11.1.min.js",
+                        "~/Scripts/jquery.flexisel.js",
+                        "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                        "~/Content/style.css",
+                        "~/Content/font-awesome.min.css"));
+        }
+    }
+}
---
+++ PixelShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PixelShop.Startup))]
+namespace PixelShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
