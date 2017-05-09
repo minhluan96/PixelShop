80a1c3e6c3c8d5fe9adb4f546708181538681b92
luan nguyen <donguyenminhluan96@gmail.com>
2017-05-09 22:06:31 +0700

    Them Model, them sp man hinh chinh, nha san xuat, danh muc

--- PixelShop/Controllers/HomeController.cs
+++ PixelShop/Controllers/HomeController.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PixelShop.Models;
+
 
 namespace PixelShop.Controllers
 {
     public class HomeController : Controller
     {
+        PixelShopEntities db = new PixelShopEntities();
+
         public ActionResult Index()
         {
+            List<SANPHAM> listTop8SanPham = db.SANPHAMs.Where(sp => sp.BiXoa == 0).Select(sp => sp).Take(8).ToList<SANPHAM>();
+            List<SANPHAM> lstSanPhamXemNhieu = db.SANPHAMs.Where(sp => sp.BiXoa == 0).OrderBy(sp => sp.SoLuotXem).Take(5).ToList<SANPHAM>();
+
+            ViewData["banChay"] = listTop8SanPham;
+            ViewData["xemNhieu"] = lstSanPhamXemNhieu;
+
             return View();
         }
     }
---
+++ PixelShop/Controllers/TimKiemController.cs
@@ -0,0 +1,43 @@
+﻿using PixelShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PixelShop.Controllers
+{
+    public class TimKiemController : Controller
+    {
+
+        PixelShopEntities db = new PixelShopEntities();
+        // GET: ListProduct
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NhaSanXuat(string id)
+        {
+            List<SANPHAM> dsSanPham = db.SANPHAMs.Where(sp => sp.BiXoa == 0).Select(sp => sp).ToList<SANPHAM>();
+            if (!String.IsNullOrEmpty(id))
+            {
+                dsSanPham = db.SANPHAMs.Where(sp => sp.NhaSanXuat.Equals(id) && sp.BiXoa == 0).Select(sp => sp).ToList<SANPHAM>();
+                return View(dsSanPham);
+            }
+            return View(dsSanPham);
+        }
+
+        public ActionResult DanhMuc(string id)
+        {
+            List<SANPHAM> lstSPDanhMuc = db.SANPHAMs.Where(sp => sp.BiXoa == 0).Select(sp => sp).ToList<SANPHAM>();
+            if (!String.IsNullOrEmpty(id))
+            {
+                lstSPDanhMuc = db.SANPHAMs.Where(sp => sp.DanhMuc.Equals(id) && sp.BiXoa == 0).Select(sp => sp).ToList<SANPHAM>();
+                return View(lstSPDanhMuc);
+            }
+            return View(lstSPDanhMuc);
+        }
+        
+    }
+}
