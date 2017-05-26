303071a402cb8aca7516c08e496c5bf9c60849da
luan nguyen <donguyenminhluan96@gmail.com>
2017-05-27 00:19:22 +0700

    them man hinh lich su giao dich, fix bug, chinh css

--- PixelShop/Controllers/LishSuMuaHangController.cs
+++ PixelShop/Controllers/LishSuMuaHangController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PagedList;
+using PixelShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,15 @@ namespace PixelShop.Controllers
     public class LishSuMuaHangController : Controller
     {
         // GET: LishSuMuaHang
-        public ActionResult Index()
+        PixelShopEntities db = new PixelShopEntities();
+        public ActionResult Index(int ?page)
         {
-            return View();
+            string maNgDat = "admin@deptrai";
+            List<DONHANG> dsDH = db.DONHANGs.Where(d => d.EmailDat.Equals(maNgDat)).Select(d => d).ToList();
+            int pageSize = 4;
+            int pageNumber = (page ?? 1);
+
+            return View(dsDH.ToPagedList(pageNumber, pageSize));
         }
     }
 }
